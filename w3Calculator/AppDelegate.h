b8009a1348465e5efe4863fcd79a502c220395bc
Kugak <kugarajk@sheridanc.on.ca>
2017-01-25 09:42:27 -0500

    Initial Commit

---
+++ w3Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  w3Calculator
+//
+//  Created by Kuga Kugaraja on 2017-01-25.
+//  Copyright © 2017 Kuga Kugaraja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ w3Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  w3Calculator
+//
+//  Created by Kuga Kugaraja on 2017-01-25.
+//  Copyright © 2017 Kuga Kugaraja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
